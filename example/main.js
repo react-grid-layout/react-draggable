319106f842ee05336e95f6fdb41137ef2b172a43
Matt Zabriskie <mzabriskie@gmail.com>
2014-07-25 13:31:20 -0600

    Updating example

--- example/main.js
+++ example/main.js
@@ -52,6 +52,16 @@ var App = React.createClass({
 						<div>You must click my handle to drag me</div>
 					</div>
 				</Draggable>
+				<Draggable
+					cancel="strong"
+					onStart={this.handleStart}
+					onDrag={this.handleDrag}
+					onStop={this.handleStop}>
+					<div className="box">
+						<strong>Can't drag from here</strong>
+						<div>Dragging from here works fine</div>
+					</div>
+				</Draggable>
 			</div>
 		);
 	}
