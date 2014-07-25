7ac7840ca3a8574b930dca5e8872613844aa0577
Matt Zabriskie <mzabriskie@gmail.com>
2014-07-25 11:44:32 -0600

    Updating example

--- example/main.js
+++ example/main.js
@@ -2,11 +2,45 @@
 var React = require('react'),
 	Draggable = require('../lib/main');
 
-React.renderComponent(<div>
-	<Draggable>
-		<div className="foo">Hello</div>
-	</Draggable>
-	<Draggable>
-		<div className="foo">World</div>
-	</Draggable>
-</div>, document.body);
+var App = React.createClass({
+	handleStart: function (e, ui) {
+		console.log('start');
+	},
+
+	handleDrag: function (e, ui) {
+		console.log(ui.position);
+	},
+
+	handleStop: function () {
+		console.log('stop');
+	},
+
+	render: function () {
+		return (
+			<div>
+				<Draggable
+					onStart={this.handleStart}
+					onDrag={this.handleDrag}
+					onStop={this.handleStop}>
+					<div className="box">I can be dragged anywhere</div>
+				</Draggable>
+				<Draggable
+					axis='x'
+					onStart={this.handleStart}
+					onDrag={this.handleDrag}
+					onStop={this.handleStop}>
+					<div className="box">I can only be dragged horizonally</div>
+				</Draggable>
+				<Draggable
+					axis='y'
+					onStart={this.handleStart}
+					onDrag={this.handleDrag}
+					onStop={this.handleStop}>
+					<div className="box">I can only be dragged vertically</div>
+				</Draggable>
+			</div>
+		);
+	}
+});
+
+React.renderComponent(<App/>, document.body);
