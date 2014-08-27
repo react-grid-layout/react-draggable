823d898c487afed9e476d9298d141ee50d4b7066
cjblomqvist <carl@cjblomqvist.com>
2014-08-27 17:27:35 +0200

    Multiple things - see description
    - Do not compile example into bundle
    - add React as external dependency (already existing)
    - add process.env.NODE_ENV var to remove potential debug code (might
    actually not be needed now with less stuff in the bundle)
    - pack into library with (var) name ReactDraggable

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,25 @@
+var DefinePlugin = require('webpack').DefinePlugin;
+
 module.exports = {
-	entry: './example/main.js',
-	output: {
-		filename: './build/bundle.js'
-	},
-	module: {
-		loaders: [
-			{test: /\.js$/, loader: 'jsx-loader'}
-		]
-	}
+  entry: './lib/main.js',
+  output: {
+    filename: './build/bundle.js',
+    library: "ReactDraggable",
+    libraryTarget: "var"
+  },
+  externals: {
+    'react/addons': 'React'
+  },
+  module: {
+    loaders: [
+      {test: /\.js$/, loader: 'jsx-loader'}
+    ]
+  },
+  plugins: [
+    new DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV ? process.env.NODE_ENV : '')
+      }
+    })
+  ]
 };
