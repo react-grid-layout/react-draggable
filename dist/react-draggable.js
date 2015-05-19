246fb42f0b269db42e2d359886eb122a045ca5df
Samuel Reed <samuel.trace.reed@gmail.com>
2015-05-19 10:36:07 -0500

    Fix incorrect unminified build

--- dist/react-draggable.js
+++ dist/react-draggable.js
@@ -11,41 +11,41 @@
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
-
+/******/
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
-
+/******/
 /******/ 		// Check if module is in cache
 /******/ 		if(installedModules[moduleId])
 /******/ 			return installedModules[moduleId].exports;
-
+/******/
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
 /******/ 			exports: {},
 /******/ 			id: moduleId,
 /******/ 			loaded: false
 /******/ 		};
-
+/******/
 /******/ 		// Execute the module function
 /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-
+/******/
 /******/ 		// Flag the module as loaded
 /******/ 		module.loaded = true;
-
+/******/
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
-
-
+/******/
+/******/
 /******/ 	// expose the modules object (__webpack_modules__)
 /******/ 	__webpack_require__.m = modules;
-
+/******/
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
-
+/******/
 /******/ 	// __webpack_public_path__
 /******/ 	__webpack_require__.p = "";
-
+/******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(0);
 /******/ })
@@ -62,16 +62,16 @@ return /******/ (function(modules) { // webpackBootstrap
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
-
+	
 	var React = __webpack_require__(2);
 	var emptyFunction = function(){};
 	var assign = __webpack_require__(3);
 	var classNames = __webpack_require__(4);
-
+	
 	//
 	// Helpers. See Element definition below this section.
 	//
-
+	
 	function createUIEvent(draggable) {
 	  // State changes are often (but not always!) async. We want the latest value.
 	  var state = draggable._pendingState || draggable.state;
@@ -83,28 +83,28 @@ return /******/ (function(modules) { // webpackBootstrap
 	    }
 	  };
 	}
-
+	
 	function canDragY(draggable) {
 	  return draggable.props.axis === 'both' ||
 	      draggable.props.axis === 'y';
 	}
-
+	
 	function canDragX(draggable) {
 	  return draggable.props.axis === 'both' ||
 	      draggable.props.axis === 'x';
 	}
-
+	
 	function isFunction(func) {
 	  return typeof func === 'function' || Object.prototype.toString.call(func) === '[object Function]';
 	}
-
+	
 	// @credits https://gist.github.com/rogozhnikoff/a43cfed27c41e4e68cdc
 	function findInArray(array, callback) {
 	  for (var i = 0, length = array.length; i < length; i++) {
 	    if (callback.apply(callback, [array[i], i, array])) return array[i];
 	  }
 	}
-
+	
 	function matchesSelector(el, selector) {
 	  var method = findInArray([
 	    'matches',
@@ -115,10 +115,10 @@ return /******/ (function(modules) { // webpackBootstrap
 	  ], function(method){
 	    return isFunction(el[method]);
 	  });
-
+	
 	  return el[method].call(el, selector);
 	}
-
+	
 	/**
 	 * simple abstraction for dragging events names
 	 * */
@@ -134,10 +134,10 @@ return /******/ (function(modules) { // webpackBootstrap
 	    end: 'mouseup'
 	  }
 	};
-
+	
 	// Default to mouse events
 	var dragEventFor = eventsFor['mouse'];
-
+	
 	/**
 	 * get {clientX, clientY} positions of control
 	 * */
@@ -148,7 +148,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	    clientY: position.clientY
 	  };
 	}
-
+	
 	function addEvent(el, event, handler) {
 	  if (!el) { return; }
 	  if (el.attachEvent) {
@@ -159,7 +159,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	    el['on' + event] = handler;
 	  }
 	}
-
+	
 	function removeEvent(el, event, handler) {
 	  if (!el) { return; }
 	  if (el.detachEvent) {
@@ -170,7 +170,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	    el['on' + event] = null;
 	  }
 	}
-
+	
 	function outerHeight(node) {
 	  // This is deliberately excluding margin for our calculations, since we are using
 	  // offsetTop which is including margin. See getBoundPosition
@@ -180,7 +180,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	  height += int(computedStyle.borderBottomWidth);
 	  return height;
 	}
-
+	
 	function outerWidth(node) {
 	  // This is deliberately excluding margin for our calculations, since we are using
 	  // offsetLeft which is including margin. See getBoundPosition
@@ -197,7 +197,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	  height -= int(computedStyle.paddingBottom);
 	  return height;
 	}
-
+	
 	function innerWidth(node) {
 	  var width = node.clientWidth;
 	  var computedStyle = window.getComputedStyle(node);
@@ -205,20 +205,20 @@ return /******/ (function(modules) { // webpackBootstrap
 	  width -= int(computedStyle.paddingRight);
 	  return width;
 	}
-
+	
 	function isNum(num) {
 	  return typeof num === 'number' && !isNaN(num);
 	}
-
+	
 	function int(a) {
 	  return parseInt(a, 10);
 	}
-
+	
 	function getBoundPosition(draggable, clientX, clientY) {
 	  var bounds = JSON.parse(JSON.stringify(draggable.props.bounds));
 	  var node = draggable.getDOMNode();
 	  var parent = node.parentNode;
-
+	
 	  if (bounds === 'parent') {
 	    var nodeStyle = window.getComputedStyle(node);
 	    var parentStyle = window.getComputedStyle(parent);
@@ -232,40 +232,40 @@ return /******/ (function(modules) { // webpackBootstrap
 	      bottom: innerHeight(parent) - outerHeight(node) - node.offsetTop
 	    };
 	  }
-
+	
 	  // Keep x and y below right and bottom limits...
 	  if (isNum(bounds.right)) clientX = Math.min(clientX, bounds.right);
 	  if (isNum(bounds.bottom)) clientY = Math.min(clientY, bounds.bottom);
-
+	
 	  // But above left and top limits.
 	  if (isNum(bounds.left)) clientX = Math.max(clientX, bounds.left);
 	  if (isNum(bounds.top)) clientY = Math.max(clientY, bounds.top);
-
+	
 	  return [clientX, clientY];
 	}
-
+	
 	function snapToGrid(grid, pendingX, pendingY) {
 	  var x = Math.round(pendingX / grid[0]) * grid[0];
 	  var y = Math.round(pendingY / grid[1]) * grid[1];
 	  return [x, y];
 	}
-
+	
 	// Useful for preventing blue highlights all over everything when dragging.
 	var userSelectStyle = ';user-select: none;-webkit-user-select:none;-moz-user-select:none;' +
 	  '-o-user-select:none;-ms-user-select:none;';
-
+	
 	function addUserSelectStyles(draggable) {
 	  if (!draggable.props.enableUserSelectHack) return;
 	  var style = document.body.getAttribute('style') || '';
 	  document.body.setAttribute('style', style + userSelectStyle);
 	}
-
+	
 	function removeUserSelectStyles(draggable) {
 	  if (!draggable.props.enableUserSelectHack) return;
 	  var style = document.body.getAttribute('style') || '';
 	  document.body.setAttribute('style', style.replace(userSelectStyle, ''));
 	}
-
+	
 	function createCSSTransform(style) {
 	  // Replace unitless items with px
 	  var x = style.x + 'px';
@@ -278,19 +278,19 @@ return /******/ (function(modules) { // webpackBootstrap
 	    MozTransform: 'translate(' + x + ',' + y + ')'
 	  };
 	}
-
-
+	
+	
 	//
 	// End Helpers.
 	//
-
+	
 	//
 	// Define <Draggable>
 	//
-
+	
 	module.exports = React.createClass({
 	  displayName: 'Draggable',
-
+	
 	  propTypes: {
 	    /**
 	     * `axis` determines which axis the draggable can move.
@@ -302,7 +302,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	     * Defaults to 'both'.
 	     */
 	    axis: React.PropTypes.oneOf(['both', 'x', 'y']),
-
+	
 	    /**
 	     * `bounds` determines the range of movement available to the element.
 	     * Available values are:
@@ -338,14 +338,14 @@ return /******/ (function(modules) { // webpackBootstrap
 	      }),
 	      React.PropTypes.oneOf(['parent', false])
 	    ]),
-
+	
 	    /**
 	     * By default, we add 'user-select:none' attributes to the document body
 	     * to prevent ugly text selection during drag. If this is causing problems
 	     * for your app, set this to `false`.
 	     */
 	    enableUserSelectHack: React.PropTypes.bool,
-
+	
 	    /**
 	     * `handle` specifies a selector to be used as the handle that initiates drag.
 	     *
@@ -367,7 +367,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	     * ```
 	     */
 	    handle: React.PropTypes.string,
-
+	
 	    /**
 	     * `cancel` specifies a selector to be used to prevent drag initialization.
 	     *
@@ -389,7 +389,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	     * ```
 	     */
 	    cancel: React.PropTypes.string,
-
+	
 	    /**
 	     * `grid` specifies the x and y that dragging should snap to.
 	     *
@@ -408,7 +408,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	     * ```
 	     */
 	    grid: React.PropTypes.arrayOf(React.PropTypes.number),
-
+	
 	    /**
 	     * `start` specifies the x and y that the dragged item should start at
 	     *
@@ -430,14 +430,14 @@ return /******/ (function(modules) { // webpackBootstrap
 	      x: React.PropTypes.number,
 	      y: React.PropTypes.number
 	    }),
-
+	
 	    /**
 	     * `moveOnStartChange`, if true (default false) will move the element if the `start`
 	     * property changes.
 	     */
 	    moveOnStartChange: React.PropTypes.bool,
-
-
+	
+	
 	    /**
 	     * `zIndex` specifies the zIndex to use while dragging.
 	     *
@@ -456,7 +456,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	     * ```
 	     */
 	    zIndex: React.PropTypes.number,
-
+	
 	    /**
 	     * Called when dragging starts.
 	     * If this function returns the boolean false, dragging will be canceled.
@@ -477,7 +477,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	     * ```
 	     */
 	    onStart: React.PropTypes.func,
-
+	
 	    /**
 	     * Called while dragging.
 	     * If this function returns the boolean false, dragging will be canceled.
@@ -498,7 +498,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	     * ```
 	     */
 	    onDrag: React.PropTypes.func,
-
+	
 	    /**
 	     * Called when dragging stops.
 	     *
@@ -518,28 +518,28 @@ return /******/ (function(modules) { // webpackBootstrap
 	     * ```
 	     */
 	    onStop: React.PropTypes.func,
-
+	
 	    /**
 	     * A workaround option which can be passed if onMouseDown needs to be accessed,
 	     * since it'll always be blocked (due to that there's internal use of onMouseDown)
 	     */
 	    onMouseDown: React.PropTypes.func,
 	  },
-
+	
 	  componentWillReceiveProps: function(newProps) {
 	    // React to changes in the 'start' param.
 	    if (newProps.moveOnStartChange && newProps.start) {
 	      this.setState(this.getInitialState(newProps));
 	    }
 	  },
-
+	
 	  componentWillUnmount: function() {
 	    // Remove any leftover event handlers
-	    removeEvent(window, dragEventFor['move'], this.handleDrag);
-	    removeEvent(window, dragEventFor['end'], this.handleDragEnd);
+	    removeEvent(document, dragEventFor['move'], this.handleDrag);
+	    removeEvent(document, dragEventFor['end'], this.handleDragEnd);
 	    removeUserSelectStyles(this);
 	  },
-
+	
 	  getDefaultProps: function () {
 	    return {
 	      axis: 'both',
@@ -557,42 +557,42 @@ return /******/ (function(modules) { // webpackBootstrap
 	      onMouseDown: emptyFunction
 	    };
 	  },
-
+	
 	  getInitialState: function (props) {
 	    // Handle call from CWRP
 	    props = props || this.props;
 	    return {
 	      // Whether or not we are currently dragging.
 	      dragging: false,
-
+	
 	      // Offset between start top/left and mouse top/left while dragging.
 	      offsetX: 0, offsetY: 0,
-
+	
 	      // Current transform x and y.
 	      clientX: props.start.x, clientY: props.start.y
 	    };
 	  },
-
+	
 	  handleDragStart: function (e) {
 	    // Make it possible to attach event handlers on top of this one
 	    this.props.onMouseDown(e);
-
+	
 	    // Short circuit if handle or cancel prop was provided and selector doesn't match
 	    if ((this.props.handle && !matchesSelector(e.target, this.props.handle)) ||
 	      (this.props.cancel && matchesSelector(e.target, this.props.cancel))) {
 	      return;
 	    }
-
+	
 	    // Call event handler. If it returns explicit false, cancel.
 	    var shouldStart = this.props.onStart(e, createUIEvent(this));
 	    if (shouldStart === false) return;
-
+	
 	    var dragPoint = getControlPosition(e);
-
+	
 	    // Add a style to the body to disable user-select. This prevents text from
 	    // being selected all over the page.
 	    addUserSelectStyles(this);
-
+	
 	    // Initiate dragging. Set the current x and y as offsets
 	    // so we know how much we've moved during the drag. This allows us
 	    // to drag elements around even if they have been moved, without issue.
@@ -601,63 +601,63 @@ return /******/ (function(modules) { // webpackBootstrap
 	      offsetX: dragPoint.clientX - this.state.clientX,
 	      offsetY: dragPoint.clientY - this.state.clientY
 	    });
-
-
+	
+	
 	    // Add event handlers
-	    addEvent(window, dragEventFor['move'], this.handleDrag);
-	    addEvent(window, dragEventFor['end'], this.handleDragEnd);
+	    addEvent(document, dragEventFor['move'], this.handleDrag);
+	    addEvent(document, dragEventFor['end'], this.handleDragEnd);
 	  },
-
+	
 	  handleDragEnd: function (e) {
 	    // Short circuit if not currently dragging
 	    if (!this.state.dragging) {
 	      return;
 	    }
-
+	
 	    removeUserSelectStyles(this);
-
+	
 	    // Turn off dragging
 	    this.setState({
 	      dragging: false
 	    });
-
+	
 	    // Call event handler
 	    this.props.onStop(e, createUIEvent(this));
-
+	
 	    // Remove event handlers
-	    removeEvent(window, dragEventFor['move'], this.handleDrag);
-	    removeEvent(window, dragEventFor['end'], this.handleDragEnd);
+	    removeEvent(document, dragEventFor['move'], this.handleDrag);
+	    removeEvent(document, dragEventFor['end'], this.handleDragEnd);
 	  },
-
+	
 	  handleDrag: function (e) {
 	    var dragPoint = getControlPosition(e);
-
+	
 	    // Calculate X and Y
 	    var clientX = dragPoint.clientX - this.state.offsetX;
 	    var clientY = dragPoint.clientY - this.state.offsetY;
-
+	
 	    // Snap to grid if prop has been provided
 	    if (Array.isArray(this.props.grid)) {
 	      var coords = snapToGrid(this.props.grid, clientX, clientY);
 	      clientX = coords[0], clientY = coords[1];
 	    }
-
+	
 	    if (this.props.bounds) {
 	      var pos = getBoundPosition(this, clientX, clientY);
 	      clientX = pos[0], clientY = pos[1];
 	    }
-
+	
 	    // Call event handler. If it returns explicit false, cancel.
 	    var shouldUpdate = this.props.onDrag(e, createUIEvent(this));
 	    if (shouldUpdate === false) return this.handleDragEnd();
-
+	
 	    // Update transform
 	    this.setState({
 	      clientX: clientX,
 	      clientY: clientY
 	    });
 	  },
-
+	
 	  onMouseDown: function(ev) {
 	    // Prevent 'ghost click' which happens 300ms after touchstart if the event isn't cancelled.
 	    // We don't cancel the event on touchstart because of #37; we might want to make a scrollable item draggable.
@@ -665,22 +665,22 @@ return /******/ (function(modules) { // webpackBootstrap
 	    if (dragEventFor == eventsFor['touch']) {
 	      return ev.preventDefault();
 	    }
-
+	
 	    return this.handleDragStart.apply(this, arguments);
 	  },
-
+	
 	  onTouchStart: function(ev) {
 	    // We're on a touch device now, so change the event handlers
 	    dragEventFor = eventsFor['touch'];
-
+	
 	    return this.handleDragStart.apply(this, arguments);
 	  },
-
+	
 	  render: function () {
 	    // Create style object. We extend from existing styles so we don't
 	    // remove anything already set (like background, color, etc).
 	    var childStyle = this.props.children.props.style || {};
-
+	
 	    // Add a CSS transform to move the element around. This allows us to move the element around
 	    // without worrying about whether or not it is relatively or absolutely positioned.
 	    // If the item you are dragging already has a transform set, wrap it in a <span> so <Draggable>
@@ -690,37 +690,37 @@ return /******/ (function(modules) { // webpackBootstrap
 	      x: canDragX(this) ?
 	        this.state.clientX :
 	        0,
-
+	
 	      // Set top if vertical drag is enabled
 	      y: canDragY(this) ?
 	        this.state.clientY :
 	        0
 	    });
-
+	
 	    // Workaround IE pointer events; see #51
 	    // https://github.com/mzabriskie/react-draggable/issues/51#issuecomment-103488278
 	    var touchHacks = {
 	      touchAction: 'none'
 	    };
-
+	
 	    var style = assign({}, childStyle, transform, touchHacks);
-
+	
 	    // Set zIndex if currently dragging and prop has been provided
 	    if (this.state.dragging && !isNaN(this.props.zIndex)) {
 	      style.zIndex = this.props.zIndex;
 	    }
-
+	
 	    var className = classNames((this.props.children.props.className || ''), 'react-draggable', {
 	      'react-draggable-dragging': this.state.dragging,
 	      'react-draggable-dragged': this.state.dragged
 	    });
-
+	
 	    // Reuse the child provided
 	    // This makes it flexible to use whatever element is wanted (div, ul, etc)
 	    return React.cloneElement(React.Children.only(this.props.children), {
 	      style: style,
 	      className: className,
-
+	
 	      onMouseDown: this.onMouseDown,
 	      onTouchStart: this.onTouchStart,
 	      onMouseUp: this.handleDragEnd,
@@ -741,29 +741,29 @@ return /******/ (function(modules) { // webpackBootstrap
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
-
+	
 	function ToObject(val) {
 		if (val == null) {
 			throw new TypeError('Object.assign cannot be called with null or undefined');
 		}
-
+	
 		return Object(val);
 	}
-
+	
 	module.exports = Object.assign || function (target, source) {
 		var from;
 		var keys;
 		var to = ToObject(target);
-
+	
 		for (var s = 1; s < arguments.length; s++) {
 			from = arguments[s];
 			keys = Object.keys(Object(from));
-
+	
 			for (var i = 0; i < keys.length; i++) {
 				to[keys[i]] = from[keys[i]];
 			}
 		}
-
+	
 		return to;
 	};
 
@@ -777,17 +777,17 @@ return /******/ (function(modules) { // webpackBootstrap
 	  Licensed under the MIT License (MIT), see
 	  http://jedwatson.github.io/classnames
 	*/
-
+	
 	function classNames() {
 		var classes = '';
 		var arg;
-
+	
 		for (var i = 0; i < arguments.length; i++) {
 			arg = arguments[i];
 			if (!arg) {
 				continue;
 			}
-
+	
 			if ('string' === typeof arg || 'number' === typeof arg) {
 				classes += ' ' + arg;
 			} else if (Object.prototype.toString.call(arg) === '[object Array]') {
@@ -803,12 +803,12 @@ return /******/ (function(modules) { // webpackBootstrap
 		}
 		return classes.substr(1);
 	}
-
+	
 	// safely export classNames for node / browserify
 	if (typeof module !== 'undefined' && module.exports) {
 		module.exports = classNames;
 	}
-
+	
 	// safely export classNames for RequireJS
 	if (true) {
 		!(__WEBPACK_AMD_DEFINE_ARRAY__ = [], __WEBPACK_AMD_DEFINE_RESULT__ = function() {
@@ -821,3 +821,4 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ ])
 });
 ;
+//# sourceMappingURL=react-draggable.map
