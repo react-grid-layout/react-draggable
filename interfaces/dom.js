95b40d1f865c02ff9a9759fab0691ed8881ff5a8
Dmitry Vashchilov <dvaschilov@gmail.com>
2017-04-27 20:51:02 +0700

    Use PropTypes from "prop-types" package instead of React one (#241)

--- interfaces/dom.js
+++ interfaces/dom.js
@@ -6,6 +6,11 @@ declare class SVGElement extends HTMLElement {
 declare class TouchEvent2 extends TouchEvent {
   changedTouches: TouchList;
   targetTouches: TouchList;
-};
+}
 
 declare type MouseTouchEvent = MouseEvent & TouchEvent2;
+
+// Missing in Flow
+declare module 'prop-types' {
+    declare function exports(args: any): any;
+}
--- lib/Draggable.es6
+++ lib/Draggable.es6
@@ -1,5 +1,6 @@
 // @flow
-import React, {PropTypes} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 // $FlowIgnore
 import classNames from 'classnames';
