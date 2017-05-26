8917998fb696a9d638b1b042bac5e0cf0fbfbb2a
Heiko Behrens <mail@HeikoBehrens.net>
2017-05-25 22:47:31 -0700

    make Draggable explicit default export

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-module.exports = require('./lib/Draggable').default;
+var Draggable = require('./lib/Draggable').default;;
+module.exports = Draggable;
+module.exports.default = Draggable;
 module.exports.DraggableCore = require('./lib/DraggableCore').default;
