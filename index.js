93d4eaee45f234e0ab4ef96493cb94d21fac62d6
Heiko Behrens <mail@HeikoBehrens.net>
2017-05-25 23:00:38 -0700

    remove repetitive ;

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var Draggable = require('./lib/Draggable').default;;
+var Draggable = require('./lib/Draggable').default;
 module.exports = Draggable;
 module.exports.default = Draggable;
 module.exports.DraggableCore = require('./lib/DraggableCore').default;
