ed73d83e7ab654db6b44b5fca8e2c17150281ce5
Bogdan Chadkin <trysound@yandex.ru>
2018-04-02 12:10:51 +0300

    Perfer named exports and bundle esm and cjs
    In this diff I replaced default export with named exports for better
    tooling support.
    
    Now both `Draggable` and `DraggableCode` have the only one way to import
    it.
    
    ```js
    import { Draggable, DraggableCore } from 'react-draggable';
    ```
    
    or with commonjs
    
    ```js
    const { Draggable, DraggableCore } = require('react-draggable');
    ```
    
    Added esm and cjs bundles without bundled vendor dependencies. This
    should have 8KB reduction for consumers.
    
    Removed `browser` field in package.json because it's usually useless and
    prefered over `module` which is not what we are trying to achieve.

--- rollup.config.js
+++ rollup.config.js
@@ -4,8 +4,16 @@ import babel from 'rollup-plugin-babel';
 import replace from 'rollup-plugin-replace';
 import uglify from 'rollup-plugin-uglify';
 import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
+import pkg from './package.json';
 
-const input = './lib/umd.js';
+const input = './lib/index.js';
+
+const getBabelOptions = () => ({
+  exclude: 'node_modules/**',
+  plugins: ['external-helpers'] 
+})
+
+const isExternal = id => !id.startsWith('\0') && !id.startsWith('.') && !id.startsWith('/')
 
 export default [
   {
@@ -24,7 +32,7 @@ export default [
     plugins: [
       nodeResolve(),
       commonjs({ include: 'node_modules/**' }),
-      babel({ exclude: 'node_modules/**', plugins: ['external-helpers'] }),
+      babel(getBabelOptions()),
       replace({
         'process.env.DRAGGABLE_DEBUG': 'false',
         'process.env.NODE_ENV': JSON.stringify('development')
@@ -49,13 +57,40 @@ export default [
     plugins: [
       nodeResolve(),
       commonjs({ include: 'node_modules/**' }),
-      babel({ exclude: 'node_modules/**', plugins: ['external-helpers'] }),
+      babel(getBabelOptions()),
       replace({
         'process.env.DRAGGABLE_DEBUG': 'false',
         'process.env.NODE_ENV': JSON.stringify('production')
       }),
       sizeSnapshot(),
       uglify()
+    ]
+  },
+
+  {
+    input,
+    output: {
+      file: pkg.main,
+      format: 'cjs'
+    },
+    external: isExternal,
+    plugins: [
+      babel(getBabelOptions()),
+      replace({ 'process.env.DRAGGABLE_DEBUG': 'false' })
+    ]
+  },
+
+  {
+    input,
+    output: {
+      file: pkg.module,
+      format: 'es'
+    },
+    external: isExternal,
+    plugins: [
+      babel(getBabelOptions()),
+      replace({ 'process.env.DRAGGABLE_DEBUG': 'false' }),
+      sizeSnapshot()
     ]
   }
 ];
