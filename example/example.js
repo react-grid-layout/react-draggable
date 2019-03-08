02256716844501a491950e7911cd52f6af631472
tnrich <tnrich@teselagen.com>
2019-03-08 15:45:44 -0800

    fixing missed rename

--- example/example.js
+++ example/example.js
@@ -137,7 +137,7 @@ var App = React.createClass({
             {"I have a default position of {x: 25, y: 25}, so I'm slightly offset."}
           </div>
         </Draggable>
-        <Draggable initialPosition={{x: '-10%', y: '-10%'}} {...dragHandlers}>
+        <Draggable positionOffset={{x: '-10%', y: '-10%'}} {...dragHandlers}>
           <div className="box">
             {'I have a default position based on percents {x: \'-10%\', y: \'-10%\'}, so I\'m slightly offset.'}
           </div>
